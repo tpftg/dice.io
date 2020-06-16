ec26b5f059e72b037ef860ce7538dc54ce9f7804
Karim Slimani <karim@playmedia.fr>
2020-06-16 04:27:46 +0200

    feat(miro): use team account id to set channel name

--- src/miro/plugin.js
+++ src/miro/plugin.js
@@ -5,27 +5,38 @@ import MiroPluginApp from './miro-plugin-app'
 
 /* global DiceIO, miro */
 
+function startApp(nickname, channel) {
+  // Create dice roller client instance
+  let pathname = window.location.pathname.replace(/miro\/sidebar\.html$/gi, '')
+  let diceRoller = new DiceIO({
+    channel: channel,
+    nickname: nickname,
+    serverUrl: window.location.origin,
+    socketOptions: { path: pathname + 'socket.io' },
+  })
+
+  // Start Miro web plugin application
+  let app = new MiroPluginApp(diceRoller) // eslint-disable-line no-unused-vars
+}
+
 miro.onReady(() => {
-    // Create dice roller client instance
-    let pathname = window.location.pathname.replace(/miro\/sidebar\.html$/gi, '')
-    let diceRoller = new DiceIO({
-        serverUrl: window.location.origin,
-        socketOptions: { path: pathname + 'socket.io' },
+  // Attempt to get current user name (dice roller nickname)
+  miro.currentUser.getId()
+    .then((id) => {
+      fetch('https://miro.com/api/v1/users/' + id)
+        .then(response => response.json())
+        .then(data => {
+          // Attempt to get team account id (dice roller channel)
+          miro.account.get()
+            .then((account) => {
+              startApp(data.name, 'miro' + account.id)
+            })
+            .catch(e => {
+              console.log('[Dice Roller] Failed to get team account id', e)
+            })
+        })
+        .catch(e => {
+          console.log('[Dice Roller] Failed to get current user nickname', e)
+        })
     })
-
-    // Attempt to get current user nickname
-    miro.currentUser.getId()
-      .then((id) => {
-        fetch('https://miro.com/api/v1/users/' + id)
-          .then(response => response.json())
-          .then(data => {
-            diceRoller.setNickname(data.name)
-          })
-          .catch(e => {
-            console.log('Failed to get user nickname', e)
-          })
-      })
-
-    // Start Miro web plugin application
-    let app = new MiroPluginApp(diceRoller) // eslint-disable-line no-unused-vars
 })
