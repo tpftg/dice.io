8a29152b7cc36d1a1405c9034f59c125bfab91b2
Karim Slimani <karim@playmedia.fr>
2020-06-30 15:28:38 +0200

    fix(demo): set cookie samesite value to strict

--- src/demo/cookie.js
+++ src/demo/cookie.js
@@ -4,7 +4,7 @@ export function setCookie(cname, cvalue, exdays) {
   let d = new Date()
   d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000))
   let expires = 'expires=' + d.toUTCString()
-  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/'
+  document.cookie = cname + '=' + cvalue + ';' + expires + ';path=/;samesite=strict'
 }
 
 export function getCookie(cname) {
